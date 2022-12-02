8b6e16e9c05971c9b371a258e46bee917b6766cd
Mohnishmahakalkar <mohnishmahakalkar111@gmail.com>
2022-12-02 10:19:32 +0530

    update for type orm and postgress implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './db/database.module';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [DatabaseModule, TodoModule],
+  imports: [TodoModule],
 })
 export class AppModule {}
